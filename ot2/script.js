5037bc415f77dc72382c84cfb3610a2a446bc9ed
VICTOR HUGO COSTA CORREA <victor_h_correa@estudante.sesisenai.org.br>
2024-09-02 21:40:05 -0300

    Vue JS - aprendizado

---
+++ ot2/script.js
@@ -0,0 +1,69 @@
+// var app = new Vue ({
+//     el: '#app',
+//     data: {
+//         url: 'https://vuejs.org'
+//     }
+// });
+
+// const { VueElement } = require("vue");
+
+
+
+// var app = new Vue ({
+//     el: '#app',
+//     data: {
+//         message:''
+//     }
+// })
+
+// var app = new Vue({
+//     el: '#app',
+//     data: {
+//         seen: true
+//     }
+// })
+
+// var app = new Vue ({
+//     el: '#app',
+//     data: {
+//         status: true,
+//         titulo: 'Aprendendo Vue JS'
+//     }
+// })
+
+// var app = new Vue({
+//     el: '#app',
+//     data: {
+//         usuario: {
+//             id: 1,
+//             nome: 'Jõao',
+//             email: 'j@j.com',
+//             profissao: 'Programador'
+//         }
+//     }
+// })
+
+// var app = new Vue ({
+//     el: '#app',
+//     data: {
+//         items: [
+//             {id: 1, text: 'Item 1'},
+//             {id: 2, text: 'Item 2'},
+//             {id: 1, text: 'Item 3'}
+//         ]
+//     }
+// })
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Aprendendo Vue JS',
+        linguagens: [
+            { nome: 'HTML', status: true},
+            { nome: 'CSS', status: true},
+            { nome: 'JavaScript', status: false},
+            { nome: 'PHP', status: false},
+            { nome: 'Python', status: false},
+        ] 
+    }
+})
